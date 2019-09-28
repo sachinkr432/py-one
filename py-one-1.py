71660a77e7edc90ce44b1666822b897822acc56b
sachinkr432 <sachinkr432@gmail.com>
2019-09-28 14:52:44 +0530

    added conditions

---
+++ py-one-1.py
@@ -0,0 +1,12 @@
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's hot outside")
+    print('Take Umbrella with You')
+elif is_cold:
+    print("It's cold outside")
+    print('Wear hot clothes')
+else:
+    print ("The weather is sunny")
+print('Enjoy your day')

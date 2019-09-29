847f14be38f5a2add6dcd0fa8509e013d0d55861
sachinkr432 <sachinkr432@gmail.com>
2019-09-29 08:28:19 +0530

    Price of house with down payment

--- py-one-2.py
+++ py-one-2.py
@@ -1,7 +1,7 @@
 has_good_credit = True
 price = 1000000
 if has_good_credit:
-    price -= price * 0.1
+    dwn_pmnt = price * 0.1
 else:
-    price -= price *0.2
-print("Down payment is " + str(price))
+    dwn_pmnt = price *0.2
+print("Down payment is " + str(dwn_pmnt))
